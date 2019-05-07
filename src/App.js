2f6bead9a8f4f1ab47a92e40b61d7dc73a4b96f0
oahmaro <osamaahmaro@gmail.com>
2019-05-07 12:05:38 +0300

    Composition in React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+    return (
+      <ol>
+        {people.map(person => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ol>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactList
+        contacts={[{ name: 'Tyler' }, { name: 'Karen' }, { name: 'Richard' }]}
+      />
+      <ContactList
+        contacts={[{ name: 'Osama' }, { name: 'Ahmad' }, { name: 'Hamza' }]}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
