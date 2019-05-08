2fb99117946ff1c5534e4554afb835635c22c2f2
oahmaro <osamaahmaro@gmail.com>
2019-05-08 11:25:00 +0300

    Use API To Fetch Remote Contacts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import * as ContactAPI from './utils/ContactAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        id: 'karen',
-        name: 'Karen Isgrigg',
-        handle: 'karen_isgrigg',
-        avatarURL: 'http://localhost:5001/karen.jpg'
-      },
-      {
-        id: 'richard',
-        name: 'Richard Kalehoff',
-        handle: 'richardkalehoff',
-        avatarURL: 'http://localhost:5001/richard.jpg'
-      },
-      {
-        id: 'tyler',
-        name: 'Tyler McGinnis',
-        handle: 'tylermcginnis',
-        avatarURL: 'http://localhost:5001/tyler.jpg'
-      }
-    ]
+    contacts: []
+  }
+
+  componentDidMount() {
+    ContactAPI.getAll().then(contacts =>
+      this.setState(() => ({
+        contacts
+      }))
+    )
   }
 
   removeContact = contact => {
